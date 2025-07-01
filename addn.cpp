239d3e16cfc501828b262c03a56a188ec98ab3b8
Viswanath Rao <viswanathrao7@gmail.com>
2025-07-01 20:28:18 +0530

    selectionsort

--- addn.cpp
+++ addn.cpp
@@ -1,9 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main() {
-    int a = 11, b = 8;
+// int main() {
+//     int a = 11, b = 8;
 
-    cout << a + b;
-    return 0;
+//     cout << a + b;
+//     return 0;
+// }
+#include<bits/stdc++.n>
+int main(){
+    
 }
---
+++ prime.cpp
@@ -0,0 +1,83 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int age = 10;
+//     if (age >= 18) {
+//         cout << "You are adult" << endl;
+//     }else{
+//         cout << "You are not an adult." << endl;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int marks = 54;
+
+//     if (marks < 25) {
+//         cout << "Grade: F" << endl;
+//     } else if (marks >= 25 && marks <= 44) {
+//         cout << "Grade: E" << endl;
+//     } else if (marks >= 45 && marks <= 49) {
+//         cout << "Grade: D" << endl;
+//     } else if (marks >= 50 && marks <= 59) {
+//         cout << "Grade: C" << endl;
+//     } else if (marks >= 60 && marks <= 69) {
+//         cout << "Grade: B" << endl;
+//     } else if (marks >= 70) {
+//         cout << "Grade: A" << endl;
+//     } else {
+//         cout << "Invalid marks entered." << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// #include<bits/stdc++.h>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//      int n;
+    
+//      cin>>n;
+//      if(n%2==0){
+//             cout<<"Even"<<endl;
+//         }
+//         else{
+//             cout<<"Odd"<<endl;
+//      }
+
+//   (#include <iostream>
+using namespace std;
+void printRange(int start, int end) {
+    if (start > end) return;
+    cout << start << endl;
+    printRange(start + 1, end); 
+    include <iostream> 
+    n
+}
+}
+
+
+#include <iostream>
+using namespace std;
+void printrange(int start, int end);
+
+void printRange(int start, int end) {
+    if (start > end) return;
+    cout << start << endl;
+    printRange(start + 1, end);
+}
+
+int main() {
+    for
+    printRange(-10, 22);
+    return 0;
+}
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int cnt = 0;
